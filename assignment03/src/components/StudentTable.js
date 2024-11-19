6308b8482dbe54c0ee2c9410e8bd2f65cdf81764
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-11-19 19:21:46 +0530

    create student table

--- assignment03/src/components/StudentTable.js
+++ assignment03/src/components/StudentTable.js
@@ -1,5 +1,36 @@
-export default function StudentTable(){
+import '../assets/css/compo.css'
+export default function StudentTable(props){
     return(
+        
+          <div className="outerDiv">
+            <div className="leftDiv">
+                <table border={1}>
+                    <thead>
+                        <tr>
+                            <td>first name</td>
+                            <td>last name</td>
+                            <td>course</td>
+                            <td>country</td>
+                            <td>profile</td>
+                        </tr>
+                    </thead>
+                    <tbody>
 
+                    {
+                            props.students.map((student, key) => {
+                                return (
+                                    <tr key={key}>
+                                        <td>{student.firstName}</td>
+                                        <td>{student.lastName}</td>
+                                        <td>{student.course}</td>
+                                        <td>{student.address.country}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>       
     );
 }
